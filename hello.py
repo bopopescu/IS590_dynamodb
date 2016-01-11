4b541444ffbf24ba85c436364aab46872bf23c80
Rodney cox <rodcox89@gmail.com>
2016-01-11 09:30:49 -0700

    second commit

--- hello.py
+++ hello.py
@@ -1,6 +1,42 @@
 from flask import Flask
 app = Flask(__name__)
 
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route('/create_user')
+def create_user():
+    dynamo.users.put_item(data={
+        'username': 'rdegges',
+        'first_name': 'Randall',
+        'last_name': 'Degges',
+        'email': 'r@rdegges.com',
+    })
+
+    # or ...
+
+    dynamo.tables['users'].put_item(data={
+        'username': 'rdegges',
+        'first_name': 'Randall',
+        'last_name': 'Degges',
+        'email': 'r@rdegges.com',
+    })
+
+with app.app_context():
+    for table_name, table in dynamo.tables.iteritems():
+        print table_name, table
+
+# app.py
+
+with app.app_context():
+    dynamo.destroy_all()
+
+
+
+
 @app.route("/")
 def hello():
     return "Hello World!"
